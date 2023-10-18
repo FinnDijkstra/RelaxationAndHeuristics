3a0d02ef1b85b82bced764979700a8fb3b04568e
FinnDijkstra <finn.dijkstra@gmail.com>
2023-10-18 12:52:00 +0200

    Added other standard stuff (ty pycharm)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import gurobipy
+import gurobipy as gp
+from gurobipy import GRB
 import numpy
 import pandas
 import matplotlib
@@ -12,3 +13,46 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+
+# This example formulates and solves the following simple MIP model:
+#  maximize
+#        x +   y + 2 z
+#  subject to
+#        x + 2 y + 3 z <= 4
+#        x +   y       >= 1
+#        x, y, z binary
+
+
+
+try:
+
+    # Create a new model
+    m = gp.Model("mip1")
+
+    # Create variables
+    x = m.addVar(vtype=GRB.BINARY, name="x")
+    y = m.addVar(vtype=GRB.BINARY, name="y")
+    z = m.addVar(vtype=GRB.BINARY, name="z")
+
+    # Set objective
+    m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
+
+    # Add constraint: x + 2 y + 3 z <= 4
+    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
+
+    # Add constraint: x + y >= 1
+    m.addConstr(x + y >= 1, "c1")
+
+    # Optimize model
+    m.optimize()
+
+    for v in m.getVars():
+        print('%s %g' % (v.VarName, v.X))
+
+    print('Obj: %g' % m.ObjVal)
+
+except gp.GurobiError as e:
+    print('Error code ' + str(e.errno) + ': ' + str(e))
+
+except AttributeError:
+    print('Encountered an attribute error')
