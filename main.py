f74bdd2e6988b6ce022b56e60cbbd8cb82bea287
FinnDijkstra <finn.dijkstra@gmail.com>
2023-10-24 14:01:18 +0200

    Dat reader

--- main.py
+++ main.py
@@ -5,66 +5,80 @@ import pandas
 import matplotlib
 import sys
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+#
+# # This example formulates and solves the following simple MIP model:
+# #  maximize
+# #        x +   y + 2 z
+# #  subject to
+# #        x + 2 y + 3 z <= 4
+# #        x +   y       >= 1
+# #        x, y, z binary
+#
+#
+#
+# try:
+#
+#     # Create a new model
+#     m = gp.Model("mip1")
+#
+#     # Create variables
+#     x = m.addVar(vtype=GRB.BINARY, name="x")
+#     y = m.addVar(vtype=GRB.BINARY, name="y")
+#     z = m.addVar(vtype=GRB.BINARY, name="z")
+#
+#     # Set objective
+#     m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
+#
+#     # Add constraint: x + 2 y + 3 z <= 4
+#     m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
+#
+#     # Add constraint: x + y >= 1
+#     m.addConstr(x + y >= 1, "c1")
+#
+#     # Optimize model
+#     m.optimize()
+#
+#     for v in m.getVars():
+#         print('%s %g' % (v.VarName, v.X))
+#
+#     print('Obj: %g' % m.ObjVal)
+#
+# except gp.GurobiError as e:
+#     print('Error code ' + str(e.errno) + ': ' + str(e))
+#
+# except AttributeError:
+#     print('Encountered an attribute error')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+#m.addConstrs((x[i][j] == x[j][i] for i in range(n) for j in range(i+1,n)), name='c')
 
 
-# This example formulates and solves the following simple MIP model:
-#  maximize
-#        x +   y + 2 z
-#  subject to
-#        x + 2 y + 3 z <= 4
-#        x +   y       >= 1
-#        x, y, z binary
-
-
-
-try:
-
-    # Create a new model
-    m = gp.Model("mip1")
-
-    # Create variables
-    x = m.addVar(vtype=GRB.BINARY, name="x")
-    y = m.addVar(vtype=GRB.BINARY, name="y")
-    z = m.addVar(vtype=GRB.BINARY, name="z")
-
-    # Set objective
-    m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
-
-    # Add constraint: x + 2 y + 3 z <= 4
-    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
-
-    # Add constraint: x + y >= 1
-    m.addConstr(x + y >= 1, "c1")
-
-    # Optimize model
-    m.optimize()
-
-    for v in m.getVars():
-        print('%s %g' % (v.VarName, v.X))
-
-    print('Obj: %g' % m.ObjVal)
-
-except gp.GurobiError as e:
-    print('Error code ' + str(e.errno) + ': ' + str(e))
-
-except AttributeError:
-    print('Encountered an attribute error')
+def readDat(filename):
+    with open(filename) as datFile:
+        x = [data.split() for data in datFile]
+        n = x[0][0]
+        A = x[:][1:]
+        return n, A
 
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: python cat.py <file>")
         sys.exit(-1)
-    with open(sys.argv[1]) as infile:
-        for s in infile:
-            print(s, end = "")
+    n, A = readDat(sys.argv[1])
+    print(n)
+    print(A)
+
 
 main()
+
